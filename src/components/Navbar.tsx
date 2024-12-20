c7a8293df4e554e7acbf3d460f3c7f480b556236
pabloenzenhofer <pabloenzenhofer@gmail.com>
2024-12-20 13:27:21 -0300

    update mobile responsive design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,31 @@
-import { FaInstagram } from 'react-icons/fa'
+import { useState } from 'react'
+import { FaInstagram, FaBars, FaTimes } from 'react-icons/fa'
 import logo from '../assets/logo.png'
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <a href="#" className="navbar-logo">
           <img src={logo} alt="Logo" className="nav-logo-img" />
         </a>
-        <ul className="navbar-menu">
-          <li><a href="#inicio">Inicio</a></li>
-          <li><a href="#nosotros">Nosotros</a></li>
-          <li><a href="#diferencial">Hacemos la Diferencia</a></li>
-          <li><a href="#servicios">Servicios</a></li>
-          <li><a href="#questionnaire">Evaluación</a></li>
-          <li><a href="#articles">Artículos</a></li>
-          <li><a href="#careers">Trabajá con Nosotros</a></li>
+        <button className="menu-toggle" onClick={toggleMenu}>
+          {isMenuOpen ? <FaTimes /> : <FaBars />}
+        </button>
+        <ul className={`navbar-menu ${isMenuOpen ? 'active' : ''}`}>
+          <li><a href="#inicio" onClick={() => setIsMenuOpen(false)}>Inicio</a></li>
+          <li><a href="#nosotros" onClick={() => setIsMenuOpen(false)}>Nosotros</a></li>
+          <li><a href="#diferencial" onClick={() => setIsMenuOpen(false)}>Hacemos la Diferencia</a></li>
+          <li><a href="#servicios" onClick={() => setIsMenuOpen(false)}>Servicios</a></li>
+          <li><a href="#questionnaire" onClick={() => setIsMenuOpen(false)}>Evaluación</a></li>
+          <li><a href="#articles" onClick={() => setIsMenuOpen(false)}>Artículos</a></li>
+          <li><a href="#careers" onClick={() => setIsMenuOpen(false)}>Trabajá con Nosotros</a></li>
         </ul>
         <a 
           href="https://www.instagram.com/psicologiadelaobesidad" 
